2aece26ac789ae3a943466516fcfff6cabdc67fe
Hoàng <huyhoangbcvt@gmail.com>
2022-10-15 20:03:40 +0700

    Update admin.py

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,10 +1,10 @@
 # Register your models here.
 from django.contrib import admin
-from django.contrib.auth.models import User
-from .models.account_model import Author
-from .modules.views_auth import CustomUserAdmin
+# from django.contrib.auth.models import User
+from .models.account_model import Profile
+# from .modules.views_auth import CustomUserAdmin
 
 # Register your models here
-admin.site.register(Author)
+admin.site.register(Profile)
 # admin.site.unregister(User)
 # admin.site.register(User, CustomUserAdmin)
