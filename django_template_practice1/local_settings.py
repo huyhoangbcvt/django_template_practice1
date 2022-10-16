2132e741ae7ca9e897940c9262a4bffc45507f3b
Hoàng <huyhoangbcvt@gmail.com>
2022-10-16 16:26:25 +0700

    Update local_settings.py

--- django_template_practice1/local_settings.py
+++ django_template_practice1/local_settings.py
@@ -2,3 +2,7 @@ LOGIN_REDIRECT_URL = '/admin/'
 LOGOUT_REDIRECT_URL = '/admin/'
 LOGIN_URL = '/admin/login/'
 BREADCRUMBS_HOME_LABEL = 'Trang chủ'
+
+GOOGLE_CLIENT_ID='226127222416-2c4reo3d755cofltcev8iars4no55nch.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET='EgzjoQoUWB4ax4VIaD4WjDPB'
+GOOGLE_REDIRECT_URI='http://localhost:8083/bhcn.bh/admincallservices/loginTS24IDGoogle.html'
